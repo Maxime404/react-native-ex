bb0ba29ad65908e91ff1540611c4f5f13e4ed329
Maxime GALISSAIRE <20190218@B15-4.local>
2020-02-26 17:56:44 +0100

    WIP - ex05

--- Day02/ex05/App.js
+++ Day02/ex05/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
 import {
-  View,
-  Text
+    View,
+    Text,
+    SafeAreaView
 } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import styles from '../../assets/styles';
 
 export default class MyApp extends Component {
-  render() {
-    return (
-      <View style={styles.view}>
-        <Text style={styles.title}>Fooder</Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            markers: [
+                {
+                    latitude: 27.9879017,
+                    longitude: 86.9253141
+                }
+            ]
+        }
+    }
+
+    addMarker = (e) => {
+        this.setState({
+            markers: [...this.state.markers, {
+                latitude: e.nativeEvent.coordinate.latitude,
+                longitude: e.nativeEvent.coordinate.longitude
+            }]
+        })
+    }
+
+    render() {
+        return (
+            <SafeAreaView style={styles.safeArea}>
+                <MapView
+                    style={{ flex: 1 }}
+                    showsUserLocation={true}
+                    onLongPress={(e) => this.addMarker(e)}
+                >
+                    {this.state.markers.map((marker) =>
+                        <Marker
+                            coordinate={{
+                                latitude: marker.latitude,
+                                longitude: marker.longitude
+                            }}
+                            title={"title"}
+                            description={"description"}
+                        />
+                    )}
+                </MapView>
+
+
+            </SafeAreaView>
+        );
+    }
 }
